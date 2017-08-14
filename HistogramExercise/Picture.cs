6b9f97cb0722dd224e3f7b379e8f10a1b2775766
Alek Hristov <alek.hristov@yahoo.com>
2017-08-14 16:01:07 +0300

    Update Picture.cs

--- HistogramExercise/Picture.cs
+++ HistogramExercise/Picture.cs
@@ -8,11 +8,14 @@ namespace HistogramExercise
     internal class Picture
     {
         private string name;
+        
+        //Remove
         private string imageTopColor;
 
         public Picture(string name)
         {
             this.name = name;
+            // ??
             this.ImageTopColor = imageTopColor;
         }
         public string ImageTopColor { get; set; }
@@ -23,9 +26,11 @@ namespace HistogramExercise
             {
                 GetPixels(bitmap, dictOfColors, topColors, histogram);
             }
+            
             return dictOfColors;
         }
 
+        //Get rid of th Histogram dependency.
         private void GetPixels(Bitmap bitmap, Dictionary<BasicColor, int> dictOfColors, Dictionary<string, int> topColors, Histogram histogram)
         {
             for (int x = 0; x < bitmap.Width; x++)
@@ -37,10 +42,13 @@ namespace HistogramExercise
                     FindingTheNearestColor(pixel, dictOfColors, topColors);
                 }
             }
+            
             foreach (var color in topColors.OrderByDescending(a => a.Value).Take(1))
             {
                 imageTopColor = color.Key;
                 ImageTopColor = imageTopColor;
+                
+                //Get rid of th Histogram dependency.
                 histogram.FillDataInExcelFile(imageTopColor, name);
             }
         }
@@ -67,22 +75,19 @@ namespace HistogramExercise
                     currentColor = (basicColor.Name).ToString();
                 }
             }
+            
             if (!topColors.ContainsKey(currentColor))
             {
                 topColors[currentColor] = 0;
-            }
-            if (topColors.ContainsKey(currentColor))
-            {
-                topColors[currentColor]++;
-            }
+            }                
+            
             if (!dictOfColors.ContainsKey(nearestColor))
             {
                 dictOfColors[nearestColor] = 0;
             }
-            if (dictOfColors.ContainsKey(nearestColor))
-            {
-                dictOfColors[nearestColor]++;
-            }
+            
+            topColors[currentColor]++;
+            dictOfColors[nearestColor]++;
         }
     }
 }
